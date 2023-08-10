32392efd7ae77228b927ef99bd545813538f3f6a
eemanDigital <eemandigitalconcept@gamil.com>
2023-08-10 11:41:22 +0100

    task 4 completed

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,5 +1,5 @@
 --  script that creates a trigger that decreases the 
---     quantity of an item after adding a new order.
+--  quantity of an item after adding a new order.
 
 Quantity in the table items can be negative.
 
